1bf80fb559b0371687f8d57aa405e460a609ee01
Muhammad Hussain <muhammadhussain@muhammads-mbp.home>
2025-01-15 16:57:59 +0000

    add shuffle method to deck and implemented tests for it

--- src/deck.js
+++ src/deck.js
@@ -8,10 +8,20 @@ class Deck {
             '2', '3', '4', '5', '6', '7', '8', '9', '10',
             'Jack', 'Queen', 'King', 'Ace'
         ];
+
         for (let suit of suits) {
             for (let value of values) {
                 this.cards.push(new Card(value, suit));
             }
+        }
+
+        this.shuffle();
+    }
+
+    shuffle() {
+        for (let i = this.cards.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.cards[i], this.cards[j]] = [this.cards[j], this.cards[i]];
         }
     }
 }
--- tests/deck.test.js
+++ tests/deck.test.js
@@ -1,6 +1,26 @@
-const Deck = require('../src/deck');
+const Deck = require("../src/deck");
 
-test('A deck should contain 52 cards', () => {
-    const deck = new Deck();
-    expect(deck.cards.length).toBe(52);
+test("A deck should contain 52 cards", () => {
+  const deck = new Deck();
+  expect(deck.cards.length).toBe(52);
+});
+
+test("A deck should have 4 Aces (one for each suit)", () => {
+  const deck = new Deck();
+  const aces = deck.cards.filter((card) => card.value === "Ace");
+  expect(aces.length).toBe(4);
+});
+
+test("Deck should shuffle cards", () => {
+  const deck1 = new Deck();
+  const deck2 = new Deck();
+
+  const order1 = deck1.cards
+    .map((card) => `${card.value} of ${card.suit}`)
+    .join(",");
+  const order2 = deck2.cards
+    .map((card) => `${card.value} of ${card.suit}`)
+    .join(",");
+
+  expect(order1).not.toBe(order2);
 });
